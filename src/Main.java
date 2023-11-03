61e8d8179264d2d570e0ce777f56b65bdd5dfe02
Sher <jeksher.media@gmail.com>
2023-11-03 16:53:01 +0300

    add src/Main.java

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 21703;
+        int bonus = 20;
+        int miles = price / bonus;
+        System.out.println(" Зачислено " + miles + " бонусных миль");
+    }
+}
